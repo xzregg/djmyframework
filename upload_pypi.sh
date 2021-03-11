bb383288174b221059e3ee3020412a5006351a52
xzr <xzregg>
2021-03-11 18:03:03 +0800

    refactor: setup pipy 上传包

--- upload_pypi.sh
+++ upload_pypi.sh
@@ -5,3 +5,10 @@ rm -rf build/
 rm -rf dist/
 python setup.py sdist build
 twine upload dist/* $*
+
+read -p "delete dict y/n?" ask
+if [ $ask == "y" ] ; then
+rm -rf dj_myframework.egg-info/
+rm -rf build/
+rm -rf dist/
+fi
