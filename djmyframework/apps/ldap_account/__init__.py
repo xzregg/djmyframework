a9a382505c6b46feac0550743b7a0bcd0007ed7a
xzr <xzregg>
2021-04-02 13:31:58 +0800

    feat: ldpa 连接

--- djmyframework/apps/ldap_account/__init__.py
+++ djmyframework/apps/ldap_account/__init__.py
@@ -1,8 +1,11 @@
+
 from framework.utils import threadpoll
+
 
 
 from .settings import LDAP_ACCOUNT_FOLLOW_START
 
 if LDAP_ACCOUNT_FOLLOW_START:
     from .ldap_server import run_ldap_server
+
     threadpoll.async_func(run_ldap_server)()
